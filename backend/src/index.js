73fef6efa2413a3200d4705a8eca42016c9e2682
joaquincernik <joaquincernik@gmail.com>
2026-03-25 17:19:42 -0300

    produccion, cambios solo en el index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import cors from 'cors'
+import path from 'path' // NATIVO DE NODE: Para manejar rutas de carpetas
+import { fileURLToPath } from 'url' // NATIVO DE NODE: Necesario para ES Modules
 import routes from './routes.js'
 import sequelize from './database/database.js';
 import './models/associations.js'
@@ -11,14 +13,26 @@ app.use(cors())
 app.use(express.json())
 app.use('/api', routes)
 
-const PORT = 3000
+// ----------------------- deploy - ---------------------------
+// Servir los archivos estáticos del frontend (Vue)
+// Asumimos que pondrás los archivos compilados de Vue en una carpeta llamada "dist"
+app.use(express.static(path.join(__dirname, 'dist')))
+// 3. Manejo de Vue Router (Modo History)
+// Cualquier petición que no sea a la API, devolverá el index.html de Vue
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
+const PORT = process.env.PORT || 3000
 
+
+//----------------------------------------------------
 const startServer = async () => {
   try {
     await sequelize.authenticate();
     console.log('Conexion a MySQL exitosa.');
 
-    await sequelize.sync({ alter: true });
+    //await sequelize.sync({ alter: true });
+    await sequelize.sync();
     console.log('Modelos sincronizados con la base de datos.');
 
     app.listen(PORT, () => {
